e0477f811b9299a563fe1a979b252c816e49883c
Flabort <Joel.ganson@rdpolytech.ca>
2024-11-15 14:55:58 -0700

    Initial commit

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace Group_Project_Chat_app.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
---
+++ README.md
@@ -0,0 +1,2 @@
+# Group_Project_Chat_app
+ Joel, Lane, and Justin present: A chat app with voice. 
